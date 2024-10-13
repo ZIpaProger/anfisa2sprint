318753cae8c066c16e5e8710759d790bc333a0b3
Gena K. <kashchenko.gena@yandex.ru>
2024-10-13 22:33:13 +0300

    last

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,11 +6,11 @@ from ice_cream.models import IceCream
 def index(request):
     template_name = 'homepage/index.html'
 
-    ice_cream_list = IceCream.objects.select_related(
-        'category', 'wrapper'
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
         ).filter(
-            is_on_main=True, is_published=True
-        ).order_by('title')
+            is_on_main=True, is_published=True, category__is_published=True
+        )
 
     context = {
         'ice_cream_list': ice_cream_list,
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -7,7 +7,6 @@ class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
     slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
     output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок отображения')
-    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         verbose_name = 'категория'
@@ -61,12 +60,19 @@ class IceCream(PublishedModel):
         related_name='ice_creams',
         verbose_name='Категория'
     )
+
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+
     toppings = models.ManyToManyField(Topping, verbose_name='Топинг')
     is_on_main = models.BooleanField(default=False, verbose_name='На главную')
-
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженые'
+        ordering = ('output_order', 'title')
     
     def __str__(self):
         return self.title
